f26058f1e311eb96cd0f711b6dd937e743f74980
John Madden <jck13mad@gmail.com>
2022-04-07 22:01:41 -0500

    Width restictions added for gsap movement, Home design cleanup for mobile

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,6 +8,7 @@ gsap.registerPlugin(ScrollTrigger);
 function Home(){
 
     useEffect(() => {
+      if (window.innerWidth >= 759) {
       gsap.from(".first-alt", {
           scale: 0,
           opacity: 0,
@@ -241,7 +242,8 @@ function Home(){
           yPercent: 100,
           opacity: 0.4
         });
-    })
+    }})
+  
 
     console.log(window.innerWidth)
 
@@ -309,7 +311,7 @@ function Home(){
             </section>
 
             {/**Arrangements */}
-            <section className="ninth pl-[14vw] relative sm/md:top-[58vh] 3xs:top-[40vh] group">
+            <section className="ninth pl-[14vw] relative sm/md:top-[58vh] 3xs:top-[52vh] group">
                 <h1 className="font-medium text-[3vw] text-white leading-tight w-1/2">Arrangements</h1>
                 <div className="border-white relative border-2 h-1 w-40"></div>
                 {/**Image placement holder */}
@@ -323,7 +325,7 @@ function Home(){
             </section>
 
             {/**Stonework */}
-            <section className="tenth pl-[14vw] relative sm/md:top-[80vh] 3xs:top-[60vh] group">
+            <section className="tenth pl-[14vw] relative sm/md:top-[80vh] 3xs:top-[75vh] group">
                 <h1 className="font-medium text-[3vw] text-white leading-tight w-1/2">Stonework</h1>
                 <div className="border-white relative border-2 h-1 w-40"></div>
                 {/**Image placement holder */}
@@ -337,7 +339,7 @@ function Home(){
             </section>
 
             {/**Accents */}
-            <section className="eleventh pl-[14vw] relative sm/md:top-[110vh] 3xs:top-[80vh] group">
+            <section className="eleventh pl-[14vw] relative sm/md:top-[110vh] 3xs:top-[97vh] group">
                 <h1 className="font-medium text-[3vw] text-white leading-tight w-1/2">Accents</h1>
                 <div className="border-white relative border-2 h-1 w-40"></div>
                 {/**Image placement holder */}
@@ -351,7 +353,7 @@ function Home(){
             </section>
 
             {/**Afghans */}
-            <section className="twelfth pl-[14vw] relative sm/md:top-[140vh] 3xs:top-[100vh] group">
+            <section className="twelfth pl-[14vw] relative sm/md:top-[140vh] 3xs:top-[120vh] group">
                 <h1 className="font-medium text-[3vw] text-white leading-tight w-1/2">Afghans</h1>
                 <div className="border-white relative border-2 h-1 w-40"></div>
                 {/**Image placement holder */}
@@ -364,7 +366,7 @@ function Home(){
                 </div>
             </section>
 
-            <section className="thirteenth relative w-full h-[20vh] flex flex-row items-center space-x-[6vw] justify-center sm/md:top-[180vh] 3xs:top-[120vh]">
+            <section className="thirteenth relative w-full h-[20vh] flex flex-row items-center space-x-[6vw] justify-center sm/md:top-[180vh] 3xs:top-[135vh]">
               <div className="border-white relative border-2 h-1 sm/md:w-40 3xs:w-20"></div>
                 <img src={divider} alt="flower icon" className="relative sm/md:h-28 3xs:h-14 sm/md:w-auto 3xs:w-[auto]"/>
                 <div className="border-white relative border-2 h-1 sm/md:w-40 3xs:w-20"></div>
